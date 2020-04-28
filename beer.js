15a38361e74c410bf709cfb2636701254a968f5e
benmeisso <58036697+benmeisso@users.noreply.github.com>
2020-04-28 18:48:43 +0200

    étape 3, 4, 5, 6, 7, 8, 9, 10

--- beer.js
+++ beer.js
@@ -1,16 +1,72 @@
-let clic;
+let clic = 0;
 var score = 0;
-var multiplicateur = 1;  
+var multiplicateur = 1;
+var prix = 50;
+var multiplicateurAff = 2;
 
-function clicBeer(){
-    score = score + 1;
-    document.getElementById('affichage').innerText = score + " Beers"
-    document.title = score + " Beers"
+function someBeer(){ // Logique
+    score = score + multiplicateur;
+    return score;
+}
+function nbBeerAff() {
+    if(score !== 1) {
+        document.getElementById('affichage').innerText = Math.round(score) + " Bières";
+        document.title = score + " Bières";
+    }
+    else {
+        document.getElementById('affichage').innerText = score + " Bière";
+        document.title = score + " Bière";
+    }
+}
 
+
+function clicBeerAff(){ // Affichage du nombre de bières
+    clic += 1;
+    someBeer();
+    nbBeerAff();
+    gestionBouton();
 }
 
 function augmenterMultiplicateur() {
-    multiplicateur = score *= 2;
-    document.getElementById('affichage').innerText = score + " Beers"
-    document.title = score + " Beers"
+    multiplicateur = multiplicateur + 1;
+    return multiplicateur;
+}
+
+function augmenterMultiplicateurAff(){
+    multiplicateurAff = multiplicateurAff + 1;
+}
+
+
+function calculPrix() {
+    prix *= 1.2;
+    return prix;
+}
+function prixAff() {
+    document.getElementById('price').innerText = "Prix = " + Math.round(prix);
+}
+
+function calculScore() {
+    score = score - prix;
+}
+
+function multiplicateurIGAff() {
+    document.getElementById('multiplier').value = "X" + multiplicateurAff;
+}
+
+function multButton() {
+    augmenterMultiplicateur();
+    augmenterMultiplicateurAff();
+    calculScore();
+    calculPrix();
+    prixAff();
+    multiplicateurIGAff();
+    nbBeerAff();
+}
+
+function gestionBouton() {
+    if(score > prix) {
+        document.getElementById('multiplier').disabled = false;
+    }
+    else
+        document.getElementById('multiplier').disabled = true;
 }
