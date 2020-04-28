1125db1725d9386c3ebc5652e5fdd4811474babe
Brian7835 <brian.danetz@gmail.com>
2020-04-28 15:58:03 +0200

    etape 5/6

--- beer.js
+++ beer.js
@@ -1,9 +1,16 @@
 let clic;
 var score = 0;
+var multiplicateur = 1;  
 
 function clicBeer(){
     score = score + 1;
     document.getElementById('affichage').innerText = score + " Beers"
     document.title = score + " Beers"
 
+}
+
+function augmenterMultiplicateur() {
+    multiplicateur = score *= 2;
+    document.getElementById('affichage').innerText = score + " Beers"
+    document.title = score + " Beers"
 }
