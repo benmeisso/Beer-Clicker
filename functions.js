ca6bc89b8485681aa70805d756e84318ea31c056
Brian Danetz <brian.danetz@gmail.com>
2020-05-07 11:23:40 +0200

    compression photos

--- functions.js
+++ functions.js
@@ -109,7 +109,7 @@ function entrerBar() {
     document.getElementById('prix').style.visibility= "visible";
     document.getElementById('multiplier').style.display= "flex";
     document.getElementById('multiplier').style.visibility= "visible";
-    document.getElementById('html').style.background = "url(images/man-beer-bar-sit-high-stool-wooden-desk/2545.jpg) center ";
+    document.getElementById('html').style.background = "url(images/man-beer-bar-sit-high-stool-wooden-desk/2545-min.jpg) center ";
     document.getElementById('html').style.margin =0;
     document.getElementById('html').style.padding=0;
     document.getElementById('html').style.webkitBackgroundSize =  "cover";
